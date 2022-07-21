2fa99202dade4899cd4c8d2b3fca069125244e28
developpeurlaravel <Developpeurlaravel@gmail.com>
2022-07-20 13:20:02 -1100

    First Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function index()
+    {
+
+        return "welcome to product page";
+
+    //    $data= Product::all();
+
+    //   return view('product',['products'=>$data]);
+    }
+}
